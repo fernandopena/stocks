c4fc67e13da605631b7970a5ec40386c320ea7fc
Peña Fernando <pena.fernan@gmail.com>
2022-01-24 12:06:25 -0300

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Fernando Pena on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
